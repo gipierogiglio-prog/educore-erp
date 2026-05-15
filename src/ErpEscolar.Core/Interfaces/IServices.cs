e3876ac0d66aeac0332287dfd66485c506aed46f
Garrinha <gipierogiglio-prog@users.noreply.github.com>
2026-05-15 21:50:35 +0000

    🐛 Remove TransferAsync from ISchoolYearService

--- src/ErpEscolar.Core/Interfaces/IServices.cs
+++ src/ErpEscolar.Core/Interfaces/IServices.cs
@@ -47,7 +47,6 @@ public interface ISchoolYearService
     Task<Services.SchoolYearResponse?> GetByIdAsync(Guid id);
     Task<Services.SchoolYearResponse> CreateAsync(Services.CreateSchoolYearRequest request);
     Task UpdateStatusAsync(Guid id, string status);
-    Task<Services.TransferResponse> TransferAsync(Services.TransferRequest request, Guid orgId);
 }
 
 public interface IEnrollmentService
